8344ae73abf21a468b4ad43055d7377dfca94c33
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-03-18 16:37:25 +0600

    Logic of saving message to separate files created

--- fileDb.ts
+++ fileDb.ts
@@ -1,11 +1,16 @@
 import {promises as fs} from 'fs';
 import path from "path";
+import {Message} from "./types";
 
 const messagesDir = path.join(__dirname, "messages");
 
 const fileDb = {
     async init() {
         await fs.mkdir(messagesDir, { recursive: true });
+    },
+    async addNewMessage(message: Message) {
+        const fileName = path.join(messagesDir, `${message.datetime.replace(/:/g, "-")}.txt`);
+        await fs.writeFile(fileName, JSON.stringify(message));
     }
 };
 
--- routers/messages.ts
+++ routers/messages.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import {Message} from "../types";
+import fileDb from "../fileDb";
 
 const messageRouter = express.Router();
+
+messageRouter.post('/', async (req, res) => {
+    const newMessage: Message = {
+        text: req.body.message,
+        datetime: new Date().toISOString()
+    };
+
+    await fileDb.addNewMessage(newMessage);
+    res.send(newMessage);
+});
 
 export default messageRouter;
